63d5fcd241b930a59cf342510e7f2fbb93ae9259
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-08-13 19:11:22 +0200

    Change and unify indentation

--- src/App.js
+++ src/App.js
@@ -25,78 +25,78 @@ class App extends Component {
 
 //initialize map after App mounts
   componentDidMount() {
-  	//in case of Goole Maps Error
-  	window.gm_authFailure = function() {
-    	alert("Sorry! Google Map did not load properly!");
-	}
+    //in case of Goole Maps Error
+    window.gm_authFailure = function() {
+      alert("Sorry! Google Map did not load properly!");
+  }
     this.initMap();
   }
 
   initMap() {
     let self = this;
-	//create new Google Map
+  //create new Google Map
     let map = new this.props.google.maps.Map(document.getElementById('map'), {
-    	zoom: 15,
-    	center: {lat: 50.863789, lng: 15.6788113}
+      zoom: 15,
+      center: {lat: 50.863789, lng: 15.6788113}
     });
     //click on map closes info window
     map.addListener('click', function() {
-    	self.closeInfoWindow();
+      self.closeInfoWindow();
     });
     //update map state (why setState was too late here?)
     this.state.map = Object.assign({map});
-	//create markers based on locations defined in state
+  //create markers based on locations defined in state
     this.state.locations.forEach((location) => {
-    	let marker = new this.props.google.maps.Marker({
-        	position: {lat: location.lat, lng: location.lng},
-        	title: location.title,
-        	key: location.key,
-        	map: map,
-        	animation: this.props.google.maps.Animation.DROP,
-        	pageId: location.pageId
-      	});
-    	//update markers in state
-    	this.state.markers.push(marker);
-    	// open infowindow when marker is clicked
-    	marker.addListener('click', function() {
-        	self.displayInfoWindow(map, marker);
-      	});
+      let marker = new this.props.google.maps.Marker({
+          position: {lat: location.lat, lng: location.lng},
+          title: location.title,
+          key: location.key,
+          map: map,
+          animation: this.props.google.maps.Animation.DROP,
+          pageId: location.pageId
+        });
+      //update markers in state
+      this.state.markers.push(marker);
+      // open infowindow when marker is clicked
+      marker.addListener('click', function() {
+          self.displayInfoWindow(map, marker);
+        });
     });
   }
 
 //display infoWindow - func. trigerred by clicking on marker and location from list
-	displayInfoWindow = (map, marker) => {
-	    this.state.infoWindow.setContent(`<h3>${marker.title}</h3>`)
-	    this.state.infoWindow.open(map, marker)
-	    this.getWikipediaInfo(marker);
-	}
+  displayInfoWindow = (map, marker) => {
+      this.state.infoWindow.setContent(`<h3>${marker.title}</h3>`)
+      this.state.infoWindow.open(map, marker)
+      this.getWikipediaInfo(marker);
+  }
 
 //close info window - triggered by cliking on a map
-	closeInfoWindow() {
-		this.state.infoWindow.close()
-	}
+  closeInfoWindow() {
+    this.state.infoWindow.close()
+  }
 
 //fetch data from Wikipedia to populate info window
-	getWikipediaInfo(marker) {
-    	let self = this;
-    	let pageId = marker.pageId;
-    	let url = 'https://cors-anywhere.herokuapp.com/https://pl.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&pageids=' + pageId;
-    	// ew. explaintext=&
-    	fetch(url)
-    	.then(function(response){
-      		return response.json();
-    	}).then(function(data){
-      		let title = data.query.pages[pageId].title;
-      		let content = data.query.pages[pageId].extract;
-      		self.state.infoWindow.setContent(`<p>From Wikipedia</p><h3>${title}</h3><p>${content}<p>`);
-    	}).catch(function(error){
-      		alert('Failed to load Wikipedia resources ' + error);
-    	})
+  getWikipediaInfo(marker) {
+      let self = this;
+      let pageId = marker.pageId;
+      let url = 'https://cors-anywhere.herokuapp.com/https://pl.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&pageids=' + pageId;
+      // ew. explaintext=&
+      fetch(url)
+      .then(function(response){
+          return response.json();
+      }).then(function(data){
+          let title = data.query.pages[pageId].title;
+          let content = data.query.pages[pageId].extract;
+          self.state.infoWindow.setContent(`<p>From Wikipedia</p><h3>${title}</h3><p>${content}<p>`);
+      }).catch(function(error){
+          alert('Failed to load Wikipedia resources ' + error);
+      })
   }
 
   //click on button shows or hides list
   toggleListView = () => {
-	this.state.showList ? this.setState({showList: false}) : this.setState({showList: true});
+  this.state.showList ? this.setState({showList: false}) : this.setState({showList: true});
   }
 
 
@@ -106,7 +106,7 @@ class App extends Component {
       <div className="app">
               {this.state.showList ? <FilterList tabIndex="0" locations={this.state.locations} markers={this.state.markers} map={this.state.map} maps={this.props.google.maps} displayInfoWindow={this.displayInfoWindow.bind(this)} /> : null}
           <div className="map-container">
-          	<button className="toggle-button" onClick={this.toggleListView}> Hide/Show Locations List </button>
+            <button className="toggle-button" onClick={this.toggleListView}> Hide/Show Locations List </button>
             <div id="map" role="application" tabIndex="0"></div>
           </div>
       </div>
--- src/List.js
+++ src/List.js
@@ -3,86 +3,86 @@ import ecsapeRegExp from 'escape-string-regexp';
 
 
 class FilterList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			query: '',
-			locations: [],
-			filteredLocations: [],
-			markers: []
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      locations: [],
+      filteredLocations: [],
+      markers: []
+    };
+  }
 
 //update state from received props
-	componentDidMount() {
-		this.setState({locations: this.props.locations, markers: this.props.markers });
-	}
+  componentDidMount() {
+    this.setState({locations: this.props.locations, markers: this.props.markers });
+  }
 
 
-	filter = (query) => {
-		//update query
-		this.setState({query: query});
+  filter = (query) => {
+    //update query
+    this.setState({query: query});
 
-		//if search input is clear
-			if (query.length === 0) {
-				this.resetQuery();
+    //if search input is clear
+      if (query.length === 0) {
+        this.resetQuery();
 
-			} else {
-				let filteredLocations = [];
-				let filteredMarkers = [];
+      } else {
+        let filteredLocations = [];
+        let filteredMarkers = [];
 
-				const match = new RegExp(ecsapeRegExp(query), 'i');
+        const match = new RegExp(ecsapeRegExp(query), 'i');
 
-				//filter locations, add them to the array and update state
-				filteredLocations = this.state.locations.filter((location) => match.test(location.title));
-				this.setState({locations: filteredLocations});
+        //filter locations, add them to the array and update state
+        filteredLocations = this.state.locations.filter((location) => match.test(location.title));
+        this.setState({locations: filteredLocations});
 
-				//filter markers and add them to an array
-				filteredMarkers = this.state.markers.filter((marker) => match.test(marker.title));
+        //filter markers and add them to an array
+        filteredMarkers = this.state.markers.filter((marker) => match.test(marker.title));
 
-				//hide all markers and show only filtered ones, apply animation
-				this.state.markers.forEach((marker) => marker.setVisible(false));
-				filteredMarkers.forEach((marker) => {
-					marker.setVisible(true);
-					marker.setAnimation(this.props.maps.Animation.BOUNCE);
-				});
-			}
-	}
+        //hide all markers and show only filtered ones, apply animation
+        this.state.markers.forEach((marker) => marker.setVisible(false));
+        filteredMarkers.forEach((marker) => {
+          marker.setVisible(true);
+          marker.setAnimation(this.props.maps.Animation.BOUNCE);
+        });
+      }
+  }
 
-	// reset query to show all locations and markers, stop animation
-	resetQuery = () => {
-		this.setState({query: '', locations: this.props.locations, markers: this.props.markers });
-		this.state.markers.forEach((marker) => {
-			marker.setVisible(true);
-			marker.setAnimation(null);
-		});
+  // reset query to show all locations and markers, stop animation
+  resetQuery = () => {
+    this.setState({query: '', locations: this.props.locations, markers: this.props.markers });
+    this.state.markers.forEach((marker) => {
+      marker.setVisible(true);
+      marker.setAnimation(null);
+    });
 
-	}
+  }
 
-	// when location is clicked, add animation to corresponding marker and open info window
-	handleLocationClick = (location) => {
-		let locationKey = location.getAttribute('id');
-		let activeMarker = this.state.markers.filter((marker)=>  marker.key == locationKey);
+  // when location is clicked, add animation to corresponding marker and open info window
+  handleLocationClick = (location) => {
+    let locationKey = location.getAttribute('id');
+    let activeMarker = this.state.markers.filter((marker)=>  marker.key == locationKey);
 
-		activeMarker[0].setAnimation(this.props.maps.Animation.BOUNCE)
-		setTimeout(activeMarker[0].setAnimation(null), 10000);
+    activeMarker[0].setAnimation(this.props.maps.Animation.BOUNCE)
+    setTimeout(activeMarker[0].setAnimation(null), 10000);
 
-		this.props.displayInfoWindow(this.props.map, activeMarker[0]);
-	}
+    this.props.displayInfoWindow(this.props.map, activeMarker[0]);
+  }
 
 
-	render() {
-		return(
-			<div className="list-container">
-				<h1>Discover Cieplice</h1>
-				<input className="location-serach" type="text" placeholder="Search location" tabIndex="0" aria-labelledby="search location" value={this.state.query} onChange={(event)=> this.filter(event.target.value)}/>
-				<button onClick={this.resetQuery}>X </button>
-				<ul className = "locations-list" aria-label="List of locations">
-					{this.state.filteredLocations.length === 0 ? this.state.locations.map(location => (<li id={location.key} key={location.key} tabIndex="0" role="button" onClick={(event) => this.handleLocationClick(event.target)}> {location.title} </li>)) : this.state.filteredLocations.map(location => (<li id={location.key} key={location.key} tabIndex="0" role="button" onClick={(event) => this.handleLocationClick(event.target)} > {location.title} </li>))}
-				</ul>
-			</div>
-		);
-	}
+  render() {
+    return(
+      <div className="list-container">
+        <h1>Discover Cieplice</h1>
+        <input className="location-serach" type="text" placeholder="Search location" tabIndex="0" aria-labelledby="search location" value={this.state.query} onChange={(event)=> this.filter(event.target.value)}/>
+        <button onClick={this.resetQuery}>X </button>
+        <ul className = "locations-list" aria-label="List of locations">
+          {this.state.filteredLocations.length === 0 ? this.state.locations.map(location => (<li id={location.key} key={location.key} tabIndex="0" role="button" onClick={(event) => this.handleLocationClick(event.target)}> {location.title} </li>)) : this.state.filteredLocations.map(location => (<li id={location.key} key={location.key} tabIndex="0" role="button" onClick={(event) => this.handleLocationClick(event.target)} > {location.title} </li>))}
+        </ul>
+      </div>
+    );
+  }
 
 }
 
